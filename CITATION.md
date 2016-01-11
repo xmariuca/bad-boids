72728c1d7503d8e1e22c766683a6d531e268fb90
Maria Robu <ucabmrr@ucl.ac.uk>
2016-01-11 16:42:36 +0000

    created basic file structure + cleaned code with pep8 linter

---
+++ CITATION.md
@@ -0,0 +1,4 @@
+If you wish to use this code, please cite the module MPHYG001 - Research Software Engineering with Python UCL:
+http://development.rc.ucl.ac.uk/training/engineering
+
+Author: Maria Ruxandra Robu
---
+++ setup.py
@@ -0,0 +1,15 @@
+# from setuptools import setup, find_packages
+# setup(
+#     name = "GoodBoids",
+#     version = "0.1",
+#     packages = find_packages(exclude=['*tests']),
+#     scripts = ['scripts/getBoids'],
+#     install_requires = [
+#         'argparse',
+#         'matplotlib',
+#         'geopy',
+#         'numpy',
+#         'mock',
+#         'nose'
+#         ]
+# )
