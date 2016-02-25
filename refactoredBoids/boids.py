16114d7f1c6f67bf57a97403ca067382d682f077
Maria Robu <ucabmrr@ucl.ac.uk>
2016-02-25 09:41:38 +0000

    added numpy

--- refactoredBoids/boids.py
+++ refactoredBoids/boids.py
@@ -4,13 +4,20 @@ An implementation of the boids project - refactored
 
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import numpy as np
 import random
 
 numberOfBirds = 50
-limitsPositionX = [-450.0, 50.0]
-limitsPositionY = [300.0, 600.0]
-limitsVelocityX = [0, 10.0]
-limitsVelocityY = [-20.0, 20.0]
+limitsPositionX = np.array([-450.0, 50.0])
+limitsPositionY = np.array([300.0, 600.0])
+limitsVelocityX = np.array([0, 10.0])
+limitsVelocityY = np.array([-20.0, 20.0])
+
+# create function to generate random numbers for position & velocity
+# def new_boids(count, lower_limits, upper_limits):
+#     width = upper_limits - lower_limits
+#     return (lower_limits(:,np.newaxis) + np.random.rand(2,count) * width[]:,np.newaxis])
+
 
 # position x,y for each boid
 boids_x = [random.uniform(limitsPositionX[0], limitsPositionX[1]) for x in range(numberOfBirds)]
@@ -26,37 +33,37 @@ def update_boids(boids):
     formationMaxLimit = 10000
     strengthToMiddle = 0.01
     strenghtToFormation = 0.125
-    xs, ys, xvs, yvs = boids
+    positionX, positionY, velocityX, velocityY = boids
     # Fly towards the middle
     for i in range(numberOfBirds):
         for j in range(numberOfBirds):
             # for each boid i and each boid j
-            xvs[i] = xvs[i] + (xs[j] - xs[i]) * strengthToMiddle / numberOfBirds
+            velocityX[i] = velocityX[i] + (positionX[j] - positionX[i]) * strengthToMiddle / numberOfBirds
     for i in range(numberOfBirds):
         for j in range(numberOfBirds):
-            yvs[i] = yvs[i] + (ys[j] - ys[i]) * strengthToMiddle / numberOfBirds
+            velocityY[i] = velocityY[i] + (positionY[j] - positionY[i]) * strengthToMiddle / numberOfBirds
     # Fly away from nearby boids
     for i in range(numberOfBirds):
         for j in range(numberOfBirds):
             # is the distance (ssd) is lower than a critical value
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < collisionAlert:
-                xvs[i] = xvs[i] + (xs[i] - xs[j])
-                yvs[i] = yvs[i] + (ys[i] - ys[j])
+            if (positionX[j] - positionX[i]) ** 2 + (positionY[j] - positionY[i]) ** 2 < collisionAlert:
+                velocityX[i] = velocityX[i] + (positionX[i] - positionX[j])
+                velocityY[i] = velocityY[i] + (positionY[i] - positionY[j])
     # Try to match speed with nearby boids
     for i in range(numberOfBirds):
         for j in range(numberOfBirds):
             # match the speed only with the ones that are closer than 10000
             # 0.125 is the strength of the velocity
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < formationMaxLimit:
-                xvs[i] = xvs[i] + (xvs[j] - xvs[i]) * strenghtToFormation / numberOfBirds
-                yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * strenghtToFormation / numberOfBirds
+            if (positionX[j] - positionX[i]) ** 2 + (positionY[j] - positionY[i]) ** 2 < formationMaxLimit:
+                velocityX[i] = velocityX[i] + (velocityX[j] - velocityX[i]) * strenghtToFormation / numberOfBirds
+                velocityY[i] = velocityY[i] + (velocityY[j] - velocityY[i]) * strenghtToFormation / numberOfBirds
     # Move according to velocities
     for i in range(numberOfBirds):
-        xs[i] = xs[i] + xvs[i]
-        ys[i] = ys[i] + yvs[i]
+        positionX[i] = positionX[i] + velocityX[i]
+        positionY[i] = positionY[i] + velocityY[i]
 
-xAxisLimits = [-500, 1500]
-yAxisLimits = [-500, 1500]
+xAxisLimits = np.array([-500, 1500])
+yAxisLimits = np.array([-500, 1500])
 figure = plt.figure()
 axes = plt.axes(xlim=xAxisLimits, ylim=yAxisLimits)
 scatter = axes.scatter(boids[0], boids[1])
